a5094e9dff4e6bb616ee3b287f3f15e8b8defdb5
VictorCorrea <victorhugofcorrea@gmail.com>
2019-04-07 10:17:59 -0300

    Saves and loads player position

--- Assets/Player/PlayerMovement.cs
+++ Assets/Player/PlayerMovement.cs
@@ -28,4 +28,22 @@ public class PlayerMovement : MonoBehaviour
     {
 		rb.velocity = (movement * speed);
 	}
+
+	public void SavePlayer()
+	{
+		SaveSystem.SavePlayer(this);
+	}
+
+	public void LoadPlayer()
+	{
+		PlayerData data = SaveSystem.LoadPlayer();
+
+		Vector3 position;
+		position.x = data.position[0];
+		position.y = data.position[1];
+		position.z = data.position[2];
+
+		transform.position = position;
+	}
+
 }
---
+++ Assets/Scripts/SaveSystem.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.IO; //used when working with files in the OS
+using System.Runtime.Serialization.Formatters.Binary; //access the binary formatter
+
+public static class SaveSystem
+{
+  
+	public static void SavePlayer(PlayerMovement player)
+	{
+		BinaryFormatter formatter = new BinaryFormatter();
+
+		string path = Application.persistentDataPath + "/player.fun";
+
+		FileStream stream = new FileStream(path, FileMode.Create);
+
+		PlayerData data = new PlayerData(player);
+
+		formatter.Serialize(stream, data);
+		stream.Close();
+	}
+
+	public	static PlayerData LoadPlayer()
+	{
+		string path = Application.persistentDataPath + "/player.fun";
+		if (File.Exists(path))
+		{
+			BinaryFormatter formatter = new BinaryFormatter();
+			FileStream stream = new FileStream(path, FileMode.Open);
+
+			PlayerData data = formatter.Deserialize(stream) as PlayerData;
+
+			stream.Close();
+
+			return data;
+
+		}
+		else
+		{
+			Debug.LogError("Couldn't find save file in " + path);
+			return null;
+		}
+	}
+
+	/*public static void SaveCreatures(PlayerMovement player)
+	{
+		BinaryFormatter formatter = new BinaryFormatter();
+
+		string path = Application.persistentDataPath + "/creatures.fun";
+
+		FileStream stream = new FileStream(path, FileMode.Create);
+
+		FindGameObjectsWithTag("Other");
+
+Creature.transform
+
+		CreatureData data = new CreatureData(criatura);
+
+		formatter.Serialize(stream, data);
+		stream.Close();
+	}
+	*/
+
+}
