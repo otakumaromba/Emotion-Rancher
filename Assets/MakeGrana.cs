6710a6d2fa3b24a95a0803f817a4af272d60b877
otakumaromba <victorhugofcorrea@gmail.com>
2019-04-17 16:18:17 -0300

    Base for Gems added

---
+++ Assets/MakeGrana.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class MakeGrana : MonoBehaviour, IPointerClickHandler
+{
+
+	public int grana;
+	public Text textGemas;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		grana = 0;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+		textGemas.text = "Quantidade de gemas: " + grana.ToString();
+    }
+
+	public void OnPointerClick(PointerEventData pointerEventData)
+	{
+		grana += 1;
+	}
+}
---
+++ Assets/Scripts/Gems.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class Gems : MonoBehaviour
+{
+	public int gemas;
+	public Text text;
+	public GameObject fazedorDeGrana;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		//Text text = GetComponent<Text>();
+		//UpdateGemCount();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		//gemas += fazedorDeGrana.GetComponent<MakeGrana>().grana;
+
+		//UpdateGemCount();
+		//gemas = UpdateGemCount();
+		//int tempoEmSegundos = Mathf.FloorToInt(Time.timeSinceLevelLoad);
+		//text.text = "Total de gemas: " + gemas;
+		//Debug.Log(gemas);
+    }
+
+	/*public string UpdateGemCount() //aumenta a quantidade de gemas por segundo desde o carregamento da cena
+	{
+		float gemTotal = Time.timeSinceLevelLoad;
+		gemTotal = Mathf.FloorToInt(gemTotal);
+		string gemas = gemTotal.ToString();
+		return gemas;
+	}
+
+	/*public int AddGems()
+	{
+		gemIncrease = 
+	}*/
+
+}
