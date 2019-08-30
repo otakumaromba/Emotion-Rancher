932873ec43f7ee86fc8a55b7a46fab9b3789afa8
otakumaromba <victorhugofcorrea@gmail.com>
2019-08-30 05:13:52 -0300

    Added Building Menu

---
+++ Assets/ToggleActive.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleActive : MonoBehaviour
+{
+    public void ToggleStatus()
+    {
+        if (this.gameObject.activeSelf == true)
+		{
+			this.gameObject.SetActive(false);
+		}
+		else
+		{
+			this.gameObject.SetActive(true);
+		}
+    }
+}
